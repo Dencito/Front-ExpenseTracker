c78e5f2a6d0e15322c3014d788baf2b70e60d3c9
Dencito <denar@fktech.net>
2023-09-01 21:44:49 -0300

    Change name register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
-    
+
     const error = (title) => toast.error(title, {
         position: "top-right",
         autoClose: 3000,
@@ -31,7 +31,7 @@ const Register = () => {
         progress: undefined,
         theme: "dark",
     });
-    
+
     const success = (title) => toast.success(title, {
         position: "top-right",
         autoClose: 3000,
@@ -57,26 +57,26 @@ const Register = () => {
         }
         if (!email) {
             return error("Campo email vacio");
-        } else if(!email.includes("@")){
-                return error('Campo email debe de tener "@"');
-        } else if(!email.includes(".")) {
+        } else if (!email.includes("@")) {
+            return error('Campo email debe de tener "@"');
+        } else if (!email.includes(".")) {
             return error('Campo email debe de tener punto " . "');
         }
         if (!password) {
             return error("Campo password vacio");
-        } else if(password.length < 6){
+        } else if (password.length < 6) {
             return error("Campo password debe tener mas de 6 caracteres.");
         }
         try {
             const response = await fetch('https://54.172.163.47:5118/api/user/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
-        const data = await response.json()
-        data.errors !== null ? error(data?.errors[0]) : Successfull(data)
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json()
+            data.errors !== null ? error(data?.errors[0]) : Successfull(data)
         } catch (error) {
             errorCatch("Fallo el servidor")
             console.log(error)
@@ -86,7 +86,7 @@ const Register = () => {
         <div className="content-general col-10 d-flex flex-column align-items-center justify-content-center">
             <div className="d-flex flex-column justify-content-center align-items-center gap-3">
                 <PersonPinIcon sx={{ fontSize: "100px" }} />
-                <h2 className="h1 text-bold">Logeate pibe 😊</h2>
+                <h2 className="h1 text-bold">Registrate pibe 😊</h2>
             </div>
 
             <div className='col-4 mx-auto'>
@@ -100,7 +100,7 @@ const Register = () => {
                     <Button color="primary" variant="outlined" className='mt-3' type='submit'>Register</Button>
                 </form>
                 <p className="text-center">
-                I already have an account
+                    I already have an account
                     <Link to="/login" className='ms-1' style={{ color: "#d926a9" }}>Login</Link>
                 </p>
             </div>
